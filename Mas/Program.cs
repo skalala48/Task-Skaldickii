c1fd3f97f48e3c254e850ef8177003e3ea56da8c
skalala48 <danichka9@gmail.com>
2022-01-07 20:41:49 +0700

    вфывывывфывцфы' will be ignored, and an empty message aborts the commit.

--- Mas/Program.cs
+++ Mas/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] CreateArray(int count) 
+{
+    return new int[count];
+}
+void FillArray(int[] array, int minValue, int maxValue) 
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+}
+string PrintArray(int[] array) 
+{
+    int count = array.Length;
+    string res = String.Empty;
+    for (int i = 0; i < count; i++)
+    {
+        res += $"{array[i]} ";
+    }
+    return res;
+}
+
